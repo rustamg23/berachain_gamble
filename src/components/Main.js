02db92c90175c54904c8e0edd78ef102c4bc4804
rustamg23@outlook.com <rustamg23@outlook.com>
2024-04-19 18:56:07 +0400

    bg+button+text

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,21 @@ import gitbook from '../img/gitbook.png';
 import { useState } from 'react';
 
 
+function useHover() {
+  const [hovering, setHovering] = useState(false)
+  const onHoverProps = {
+    onMouseEnter: () => setHovering(true),
+    onMouseLeave: () => setHovering(false),
+  }
+  return [hovering, onHoverProps]
+}
+
 function Main() {
   const iconSizeClasses = "h-18 w-18 opacity-75 hover:opacity-100 hover:scale-110 ease-in-out duration-500";
   const iconSizeClassesMob = "h-18 w-18 max-w-max opacity-75 mx-2";
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const [buttonAIsHovering, buttonAHoverProps] = useHover()
   const handleMouseMove = (event) => {
     setCursorPos({ x: event.clientX, y: event.clientY });
   };
@@ -30,8 +39,8 @@ function Main() {
     <div className="xl:flex xl:flex-row grid grid-rows-2 justify-around items-center w-svw h-lvh bg " >
       <div className="max-w-2xl text-left xl:space-y-8 space-y-6 xl:-ml-32 m-5 xl:relative xl:h-auto flex flex-col h-90p ">
         <h1 className="md:text-80 xs:text-5xl text-4xl font-bold bowlby-font text-white xl:mb-auto " style={{ textShadow: '8px 12px 2px rgba(0, 0, 0, 0.5)' }}>JUNKY URSAS</h1>
-        <p className='text-white poppins-font md:text-xl  text-sm font-normal xl:w-9/10 w-full xl:mb-auto ' style={{ textShadow: '3px 3px 2px rgba(0, 0, 0, 0.5)' }}>Junky Ursas is an exhilarating NFT project that brings together a collection of uniquely designed bear-themed digital art pieces, each one imbued with its own quirky personality and style. This inventive series not only celebrates the creativity and diversity.</p>
-        <button className="poppins-font font-normal bg-red-500  text-white py-2 px-4 rounded-full xl:w-1/2 w-full text-2xl hover:bg-red-600 hover:scale-105 ease-in-out duration-500">GO TO APP</button>
+        <p className='text-white poppins-font md:text-xl  text-sm font-normal xl:w-9/10 w-full xl:mb-auto ' style={{ textShadow: '3px 3px 2px rgba(0, 0, 0, 0.5)' }}>Welcome to the world of Junky Ursas! We, the high Ursas, are building an ecosystem of GambleFI projects on the Berachain. Get ready for a wild ride through a universe of decentralized fun led by a team as bold and adventurous as your favorite space-out bears!</p>
+        <button className="poppins-font font-normal bg-red-500  text-white py-2 px-4 rounded-full xl:w-1/2 w-full text-2xl hover:bg-red-600 hover:scale-105 ease-in-out duration-500" {...buttonAHoverProps}>{buttonAIsHovering ? "SOON" : "GO TO APP"}</button>
         <div className="xl:flex space-x-8 w-1/2 hidden">
           <a href='https://twitter.com/JunkyUrsas' target="_blank" rel="noopener noreferrer">
             <img src={x} alt="X" className={iconSizeClasses}/>
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,102 +1,102 @@
-import React, { useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Sector } from 'recharts';
-import AnimateGroup from 'react-smooth';
+// import React, { useState } from 'react';
+// import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Sector } from 'recharts';
+// import AnimateGroup from 'react-smooth';
 
-const CustomPieChart = ({ data }) => {
-  const [activeIndex, setActiveIndex] = useState(-1);
+// const CustomPieChart = ({ data }) => {
+//   const [activeIndex, setActiveIndex] = useState(-1);
 
-  const renderActiveShape = (props) => {
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-    return (
-      <AnimateGroup
-        component="g"
-        appear={{
-          animation: 'zoom-in',
-          duration: 400
-        }}
-        enter={{
-          animation: 'zoom-in',
-          duration: 400,
-          delay: 0
-        }}
-        leave={{
-          animation: 'zoom-out',
-          duration: 400,
-          delay: 0
-        }}
-      >
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius + 10}
-          outerRadius={outerRadius + 10} // увеличение outerRadius на 10 при наведении
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-        />
-        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-          {payload.name}
-        </text>
-        <text x={cx} y={cy} dy={22} textAnchor="middle" fill="#333">
-          {`Value: ${value}`}
-        </text>
-      </AnimateGroup>
-    );
-  };
+//   const renderActiveShape = (props) => {
+//     const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+//     return (
+//       <AnimateGroup
+//         component="g"
+//         appear={{
+//           animation: 'zoom-in',
+//           duration: 400
+//         }}
+//         enter={{
+//           animation: 'zoom-in',
+//           duration: 400,
+//           delay: 0
+//         }}
+//         leave={{
+//           animation: 'zoom-out',
+//           duration: 400,
+//           delay: 0
+//         }}
+//       >
+//         <Sector
+//           cx={cx}
+//           cy={cy}
+//           innerRadius={innerRadius + 10}
+//           outerRadius={outerRadius + 10} // увеличение outerRadius на 10 при наведении
+//           startAngle={startAngle}
+//           endAngle={endAngle}
+//           fill={fill}
+//         />
+//         <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+//           {payload.name}
+//         </text>
+//         <text x={cx} y={cy} dy={22} textAnchor="middle" fill="#333">
+//           {`Value: ${value}`}
+//         </text>
+//       </AnimateGroup>
+//     );
+//   };
   
   
 
-  const onPieEnter = (_, index) => {
-    setActiveIndex(index);
-  };
+//   const onPieEnter = (_, index) => {
+//     setActiveIndex(index);
+//   };
 
-  const onPieLeave = () => {
-    setActiveIndex(-1);
-  };
+//   const onPieLeave = () => {
+//     setActiveIndex(-1);
+//   };
 
-  return (
-    <ResponsiveContainer width="100%" height={500}>
-      <PieChart>
-        {data.length > 0 ? (
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            label={(entry) => entry.name}
-            outerRadius={200}
-            innerRadius={160}
-            fill="#8884d8"
-            dataKey="share"
-            onMouseEnter={onPieEnter}
-            onMouseLeave={onPieLeave}
-            activeShape={renderActiveShape}
-          >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={entry.color}
-                stroke={index === activeIndex ? 'black' : 'none'}
-                strokeWidth={index === activeIndex ? 4 : 1}
+//   return (
+//     <ResponsiveContainer width="100%" height={500}>
+//       <PieChart>
+//         {data.length > 0 ? (
+//           <Pie
+//             data={data}
+//             cx="50%"
+//             cy="50%"
+//             label={(entry) => entry.name}
+//             outerRadius={200}
+//             innerRadius={160}
+//             fill="#8884d8"
+//             dataKey="share"
+//             onMouseEnter={onPieEnter}
+//             onMouseLeave={onPieLeave}
+//             activeShape={renderActiveShape}
+//           >
+//             {data.map((entry, index) => (
+//               <Cell
+//                 key={`cell-${index}`}
+//                 fill={entry.color}
+//                 stroke={index === activeIndex ? 'black' : 'none'}
+//                 strokeWidth={index === activeIndex ? 4 : 1}
 
-              />
-            ))}
-          </Pie>
-        ) : (
-          <Pie
-            data={[{ name: "None", value: 100 }]}
-            cx="50%"
-            cy="50%"
-            outerRadius={150}
-            fill="#ccc"
-            dataKey="value"
-          >
-            <Cell fill="#ddd" />
-          </Pie>
-        )}
-        <Tooltip />
-      </PieChart>
-    </ResponsiveContainer>
-  );
-};
+//               />
+//             ))}
+//           </Pie>
+//         ) : (
+//           <Pie
+//             data={[{ name: "None", value: 100 }]}
+//             cx="50%"
+//             cy="50%"
+//             outerRadius={150}
+//             fill="#ccc"
+//             dataKey="value"
+//           >
+//             <Cell fill="#ddd" />
+//           </Pie>
+//         )}
+//         <Tooltip />
+//       </PieChart>
+//     </ResponsiveContainer>
+//   );
+// };
 
-export default CustomPieChart;
+// export default CustomPieChart;
