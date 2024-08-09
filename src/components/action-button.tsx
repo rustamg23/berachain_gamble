109fa2f7fa7f6974ac13029b967c8b2215cd48e3
R G <rustamg23@outlook.com>
2024-08-10 00:27:54 +0300

    Update action-button.tsx

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -20,8 +20,10 @@ const ActionButton = () => {
     <button
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onClick = {() => window.location.href = "https://betabersion.junkyursas.com?code=land"}
       className="bg-[#C72626] rounded-3xl py-4 w-full max-w-[352px] text-center text-[20px] m:text-[28px] font-semibold hover:scale-[1.03] transition-all"
-    >
+      
+      >
       {text}
     </button>
   );
